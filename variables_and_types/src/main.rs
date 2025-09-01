47eaa41fae3243bd6eb17a131736eaa5ea79b21d
dicethedev <dicethedev@gmail.com>
2025-09-01 12:17:26 +0100

    Add examples demonstrating immutability, mutability, constants, and shadowing in Rust

--- variables_and_types/src/main.rs
+++ variables_and_types/src/main.rs
@@ -1,5 +1,36 @@
 
+//Immutability vs. Mutability
+fn main() {
+    let x = 10;        // immutable
+    // x = 20;         // ❌ won't compile if you uncomment this
 
+    let mut y = 10;     // mutable
+    y = 20;             // ✅ allowed
+    println!("x = {x}, y = {y}");
+}
+
+//Constant & Variables
+const SECONDS_IN_MINUTE: u32 = 60;
+
+fn main() {
+    let mut counter = 0;  
+    counter += 1; // ✅ can change because it's mutable
+
+    // SECONDS_IN_MINUTE = 100; ❌ not allowed, constants never change
+    println!("Counter = {counter}");
+    println!("Seconds in a minute = {SECONDS_IN_MINUTE}");
+}
+
+//Shadowing (with type change)
+fn main() {
+    let spaces = "   ";        // spaces is &str
+    println!("spaces as text: '{}'", spaces);
+
+    let spaces = spaces.len(); // shadows previous spaces, now usize
+    println!("spaces as number: {}", spaces);
+}
+
+//Shadowing
 fn main() {
     // let mut x = 5;
     // println!("The value of x is: {x}");
@@ -17,8 +48,5 @@ fn main() {
 
     println!("The value of x is: {x}");
     // This is a shadowing example, not a mutability example
-
-
-    
 
 }
